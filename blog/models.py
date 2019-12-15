4d7a72db594b67d9282bf61304dc9cccbf37194e
Mohammad Reza <chashm78@gmail.com>
2019-12-15 14:27:28 +0330

    all blog page completed

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -7,3 +8,13 @@ class Blog(models.Model):
     create_date=models.DateTimeField()
     body=models.TextField()
     image=models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    
+
+    def summary(self):
+        return self.body[:100]
+
+    def date_convertor(self):
+        return self.create_date.strftime('%b %d %Y')
